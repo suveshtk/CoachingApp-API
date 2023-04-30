ce4aa223db0c2e56273dd9b064a5edc862a905ab
suveshtk <34483102+suveshtk@users.noreply.github.com>
2023-04-30 21:40:34 +0800

    added materials

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express()
 const mongoose = require('mongoose');
 const User = require('./models/userModel.js');
 const Batch = require('./models/batchModel.js');
+const Material = require('./models/materialModel.js');
 
 app.use(express.json());
 
@@ -52,6 +53,29 @@ app.post('/getActiveBatches', async (req, res) => {
         const batch = await Batch.find({ isActive: true });
         if (batch.length > 0)
             res.status(200).json(batch);
+        else
+            res.status(200).json({ error: { message: 'No active batches found!' } });
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: error.message });
+    }
+})
+
+app.post('/addMaterial', async (req, res) => {
+    try {
+        const material = await Material.create(req.body);
+        res.status(200).json(material);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: { message: error.message } });
+    }
+})
+
+app.post('/getActiveMaterial', async (req, res) => {
+    try {
+        const materials = await Material.find({ isActive: true });
+        if (materials.length > 0)
+            res.status(200).json(materials);
         else
             res.status(200).json({ error: { message: 'No active batches found!' } });
     } catch (error) {
